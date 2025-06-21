477d911e586002511e59bc4627f05201641cc529
Feliphe_De_Aquinas <feliphe.felix23@gmail.com>
2025-06-20 23:17:18 -0300

    2° versão do cabeçalho e ajuste no design das perguntas

--- java_script/perguntas.js
+++ java_script/perguntas.js
@@ -1,15 +1,51 @@
 function verificarRespostas() {
-  const perguntas = document.querySelectorAll('.pergunta');
-  let acertos = 0;
+    const perguntas = document.querySelectorAll('.pergunta');
+    let acertos = 0;
 
-  perguntas.forEach(pergunta => {
-    const correta = pergunta.getAttribute('data-resposta');
-    const selecionada = pergunta.querySelector('input[type="radio"]:checked');
-    if (selecionada && selecionada.value === correta) {
-      acertos++;
+    perguntas.forEach(pergunta => {
+        const correta = pergunta.getAttribute('data-resposta');
+        let selecionada = pergunta.querySelector('input[type="radio"]:checked');
+
+        if (selecionada && selecionada.value === correta) {
+            acertos++;
+        }
+    });
+
+    const resultado = document.getElementsByClassName('resultadoQuiz');
+
+    if (acertos !== perguntas.length) {
+        window.alert(`Pontuação: ${acertos}/${perguntas.length}`)
+        
+        let continuar = window.confirm('Deseja tentar novamente? Suas respostas serão resetadas!');
+
+        if (continuar) {
+            location.reload()
+        } else {
+            perguntas.forEach(pergunta => {
+                const correta = pergunta.getAttribute('data-resposta');
+
+                let alternativas = pergunta.getElementsByTagName('ul')[0];
+                alternativas.style.backgroundImage = 'linear-gradient(to right, #e23f3f 20%, #dbdbdb)';
+
+                let certa = pergunta.getElementsByClassName(`${correta}`)[0];
+                certa.style.backgroundImage = 'linear-gradient(to right, #3FE25A 20%, #dbdbdb)';
+            });
+        }
+
+    } else {
+        window.alert(`Parabéns, você acertou todas as perguntas !!!
+            \nPontuação: ${acertos}/${perguntas.length}`)
+        perguntas.forEach(pergunta => {
+            const correta = pergunta.getAttribute('data-resposta');
+
+            let alternativas = pergunta.getElementsByTagName('ul')[0];
+            alternativas.style.backgroundImage = 'linear-gradient(to right, #e23f3f 20%, #dbdbdb)';
+
+            let certa = pergunta.getElementsByClassName(`${correta}`)[0];
+            certa.style.backgroundImage = 'linear-gradient(to right, #3FE25A 20%, #dbdbdb)';
+        });
     }
-  });
 
-  const resultado = document.getElementsByClassName('resultadoQuiz');
-  window.alert(`Você acertou ${acertos} de ${perguntas.length} perguntas.`);
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
